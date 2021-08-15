c22a04abe470c90d9d140e8abe6f2d3c431adcef
sergio9610 <sergio.uribe1023gmail.com>
2021-08-15 09:02:05 -0500

    Visualización de movimiento de carros para cada conductor(jugador)

--- app.py
+++ app.py
@@ -15,9 +15,10 @@ def main():
                 else:
                     print(Fore.RED+'\n   MENSAJE: Debe ingresar un número mayor a 0\n')
             except ValueError:
-                print('ERROR: Debe ingresar un número válido')
+                print(Fore.RED+'\n   ERROR: Debe ingresar un número válido\n')
     
     # Lista de Jugadores
+        global listaJugadores
         listaJugadores = nombreJugadores(numJugadores)
     
 
@@ -32,19 +33,45 @@ def main():
                 else:
                     print('MENSAJE: Debe ingresar un número mayor a 0 y menor a 5')
             except ValueError:
-                print('ERROR: Debe ingresar un número válido')
+                print(Fore.RED+'\n   ERROR: Debe ingresar un número válido\n')
     
     
     # Iniciación del Juego
         while True:
             try:
                 datos(numJugadores,pista,listaJugadores)
-                enter = input("\n   Click en"+Fore.MAGENTA+" ENTER "+Fore.WHITE+"para continuar")
+                enter = input("\n   Presione"+Fore.MAGENTA+" ENTER "+Fore.WHITE+"para continuar\n")
+                if True:
+                    break
+            except ValueError:
+                print('ERROR')
+            
+        while True:
+            try:
+                pistasKm = [2000,5000,10000,21000]
+                carrera(numJugadores,listaJugadores,pistasKm,pista)
+                #enter = input("\n   Click en"+Fore.RED+" ENTER "+Fore.WHITE+"para continuar")
                 if True:
                     break
             except ValueError:
                 print('ERROR')
 
-        
+
+    # Movimientos de la carrera
+        # while True:
+        #     try:
+        #         c = 1
+        #         pistas = [10000,15000,21000,42000]
+        #         carrera(numJugadores,listaJugadores,pistas,pista)
+        #         #enter = input("\n   Click en"+Fore.RED+" ENTER "+Fore.WHITE+"para continuar")
+        #         if c == 0:
+        #             break
+        #         else:
+        #             print('SIGAA')
+        #     except ValueError:
+        #         print('ERROR')
+
+
+
 # ------- Ejecución de aplicación -------
 main()
--- modulos.py
+++ modulos.py
@@ -1,7 +1,8 @@
 from os import system
 from colorama import Fore
+import random
 
-# ------- Figura -------
+# ---------- Figura ----------
 def figura():
     #Título
     system("cls")    
@@ -13,16 +14,16 @@ def figura():
     print(Fore.WHITE+"        Configuración del Juego")
     print(Fore.BLUE+"   =================================\n")
 
-# ------- Número de Jugadores -------
+# ---------- Número de Jugadores ----------
 def numeroJugadores():
     jugadores = int(input(Fore.WHITE+"   Ingrese la cantidad de jugadores: "))
     return jugadores
 
 
-# ------- Nombre de Jugadores -------
+# ---------- Nombre de Jugadores ----------
 def nombreJugadores(numeroJugadores):
     figura()
-    global listaJugadores
+    global listaJugadores # se define globalmente para luego ser llamado
     listaJugadores = [];
     
     for i in range(numeroJugadores):
@@ -31,32 +32,102 @@ def nombreJugadores(numeroJugadores):
     return listaJugadores
 
 
-# ------- Pistas -------
+# ---------- Pistas ----------
 def pistas():
     figura()
     print(Fore.BLUE+"   =================================")
     print(Fore.WHITE+"            Escoja la Pista")
     print(Fore.BLUE+"   =================================\n")
+    print(Fore.WHITE+"   Pista 1. 2km ")
+    print(Fore.WHITE+"   Pista 2. 5km ")
+    print(Fore.WHITE+"   Pista 3. 10km ")
+    print(Fore.WHITE+"   Pista 4. 21km ")
 
-    #pistas = [10000,15000,21000,42000]
-    print(Fore.WHITE+"   Pista 1. 10km ")
-    print(Fore.WHITE+"   Pista 2. 15km ")
-    print(Fore.WHITE+"   Pista 3. 21km ")
-    print(Fore.WHITE+"   Pista 4. 42km ")
 
-#------- Datos Carrera ------
+# ---------- Datos Carrera ----------
 def datos(numJugadores,pista,listaJugadores):
-    #figura()
     print(Fore.BLUE+"\n   =================================")
     print(Fore.WHITE+"        Datos de la Carrera")
     print(Fore.BLUE+"   =================================\n")
     print(Fore.WHITE+"   Número de jugadores: "+Fore.BLUE+str(numJugadores)+Fore.WHITE+"\n   Pista: "+Fore.BLUE+str(pista))
-    #print("pista-> "+str(pista)+"      longitud-> ")
     print("\n   Conductor       Carro          Carril\n")
-    for i in range(numJugadores):
-        print(Fore.WHITE+"   "+listaJugadores[i],end="")
-        for j in range(18-len(listaJugadores[i])):
+    for numero in range(numJugadores):
+        print(Fore.WHITE+"   "+listaJugadores[numero],end="")
+        for jugador in range(18-len(listaJugadores[numero])):
             print(" ",end="")
-        print(str(i+1),end="")
-        print("              "+str(i+1))
+        print(str(numero+1),end="")
+        print("              "+str(numero+1))
     
+# ---------- Carrera ----------
+def carrera(numJugadores,listaJugadores,pistas,pista):
+    listaCarriles = []
+    # Se añaden carriles a partir del número de jugadores con el valor de la pista
+    for i in range(int(numJugadores)): 
+            listaCarriles.append(pistas[pista-1])  
+    
+    bandera = 0 # bandera de finalización
+    listaPodio = [] 
+    
+    # Ciclo principal para completación de llegada
+    while bandera == 0: 
+        listaAux = listaJugadores[:] # lista auxiliar para asignación de turnos
+    
+    # For que barre los jugadores y movimientos del carro
+        auxVisual = 0   # variable auxiliar para visualizar los movimientos generados
+        for i in range(numJugadores): 
+            turno = random.choice(listaAux) # generación del turno aleatorio
+            listaAux.remove(turno) # solo se debe mover un jugador por turno
+            movimiento = random.randint(1,6)*100 # generación del movimiento aleatorio (dado)
+            
+            if auxVisual%2 == 0:
+                print("\n    JUGADOR: "+Fore.BLUE+turno)
+                if movimiento == 100:
+                    print(Fore.WHITE+"              ___")
+                    print(Fore.WHITE+"            __| (|__")
+                    print(Fore.WHITE+"---> 100m  |_()_()_|")
+                elif movimiento == 200:
+                    print(Fore.WHITE+"                   ___")
+                    print(Fore.WHITE+"                 __| (|__")
+                    print(Fore.WHITE+"----> 200m      |_()_()_|")
+                elif movimiento == 300:
+                    print(Fore.WHITE+"                      ___")
+                    print(Fore.WHITE+"                    __| (|__")
+                    print(Fore.WHITE+"----> 300m         |_()_()_|")
+                elif movimiento == 400:
+                    print(Fore.WHITE+"                         ___")
+                    print(Fore.WHITE+"                       __| (|__")
+                    print(Fore.WHITE+"----> 400m            |_()_()_|")
+                elif movimiento == 500:
+                    print(Fore.WHITE+"                            ___")
+                    print(Fore.WHITE+"                          __| (|__")
+                    print(Fore.WHITE+"-----> 500m              |_()_()_|")
+                elif movimiento == 600:
+                    print(Fore.WHITE+"                               ___")
+                    print(Fore.WHITE+"                             __| (|__")
+                    print(Fore.WHITE+"-----> 600m                 |_()_()_|")
+                
+                enter = input("\n   Presione"+Fore.GREEN+" ENTER "+Fore.WHITE+"para continuar")
+            auxVisual = auxVisual + 1
+            
+            # Verificaciónd de finalización del carril
+            if(listaCarriles[listaJugadores.index(turno)] > 0): 
+                listaCarriles[listaJugadores.index(turno)] = listaCarriles[listaJugadores.index(turno)]-movimiento # actualización del recorrido 
+            
+            if(listaCarriles[listaJugadores.index(turno)] <= 0):  
+                
+                # Verificaición del jugador en el podio
+                banderaPodio = turno in listaPodio 
+                if banderaPodio == False: 
+                    listaPodio.append(turno) 
+        
+        # Confirmación y verificación de carros en la meta
+        carrosMeta = 0 
+        for i in range(numJugadores): 
+            if listaCarriles[i] <= 0: 
+                carrosMeta = carrosMeta+1 # actualización de carros en la meta
+
+        if carrosMeta == numJugadores: 
+            bandera = 1  # finalización de carrera
+    
+    
+    return listaPodio
